ceb3774ce09bf102e2b6d4dda49bbd563be3afaa
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-03-21 10:40:08 -0300

    1 file modified, 2 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { Header } from './components/Header/index';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="menu">
+      <h1> Logo </h1>
+      <nav>
+        <h1>Welcome to Vte!!</h1>
+        <a href="">Login</a>
+      </nav>
+    </header>
+  );
+};
+
+export { Header };

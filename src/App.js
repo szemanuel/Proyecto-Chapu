b54913f63805dc7013b0d1a6dba9472f194f1f37
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-03-21 14:40:09 -0300

    4 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './style.css';
 import { Header } from './components/Header/index';
 import { Body } from './components/Body/index';
+import { Footer } from './components/Footer/index';
+
 
 export default function App() {
   return (
     <div>
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="page">
       <header className="menu">
-        <h1> Vte </h1>
+        <h1 className="logo"> Vte </h1>
         <h1 className="welcome">Welcome to Vte!!</h1>
         <a href="#">Login</a>      
       </header>

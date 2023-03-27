c891cbd01e47229c2269c757c3350b06bd06d7ab
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-03-27 12:03:53 -0300

    3 files modified, 2 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Header } from './components/Header/index';
 import { Body } from './components/Body/index';
 import { Footer } from './components/Footer/index';
 import { Login } from './pages/Login/index';
-import { Signup } from './components/Signup/index';
+import { Signup } from './pages/Signup/index';
 
 export default function App() {
   return (
@@ -17,7 +17,7 @@ export default function App() {
         <Routes>
           <Route path="/Login" element={<Login />} />
         </Routes>
-        </BrowserRouter>
+      </BrowserRouter>
 
       {/* <Route path="/welcome"> <Welcome /></Route> */}
       {/* <Route path="/welcome"> <Welcome /></Route> */}
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Login.css';
+import { Signup } from '/pages/Signup';
+import { BrowserRouter, Route, Routes, NavLink, Link } from 'react-router-dom';
 
 const Login = () => {
   return (
@@ -7,21 +9,25 @@ const Login = () => {
       <header className="menu">
         <h1 className="logo"> Vte </h1>
         <h1 className="welcome">Welcome to Vte!!</h1>
-        <h1></h1>      
-      </header>  
-        
+        <h1></h1>
+      </header>
+
       <main className="userLogin">
         <div className="loginInputs">
-        <input className="inputType" placeholder="Email"/> 
-        <input className="inputType" placeholder="Password"/> 
+          <input className="inputType" placeholder="Email" />
+          <input className="inputType" placeholder="Password" />
         </div>
         <div className="userOptions">
-        <a href="#">Login</a>      
-        <a href="#">Sign Up</a>      
-        <a href="#">Forget Password</a>
+          <BrowserRouter>
+            <Link to="/signup">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/signup">Forget Password</Link>
+            <Routes>
+              <Route exact path="/signup" element={<Signup />}></Route>
+            </Routes>
+          </BrowserRouter>
         </div>
       </main>
-
     </div>
   );
 };

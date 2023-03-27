377d2e9a5e558474a0e474635acd736dbf85c2f4
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-03-27 11:04:45 -0300

    3 files modified, 2 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import './style.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header/index';
 import { Body } from './components/Body/index';
 import { Footer } from './components/Footer/index';
-import { Login } from './components/Login/index';
+import { Login } from './pages/Login/index';
 import { Signup } from './components/Signup/index';
-
 
 export default function App() {
   return (
     <div>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/Login" element={<Login />}/>
+        </Routes>
+      </BrowserRouter>
       <Body />
       <Footer />
-      <Login />
-      <Signup />
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
 
+      {/* <Route path="/welcome"> <Welcome /></Route> */}
+      {/* <Route path="/welcome"> <Welcome /></Route> */}
+      {/* <Route path="/welcome"> <Welcome /></Route> */}
+      {/* <Route path="/welcome"> <Welcome /></Route> */}
+      {/* <Route path="/welcome"> <Welcome /></Route> */}
+      {/* <Route path="/welcome"> <Welcome /></Route> */}
     </div>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import {NavLink} from "react-router-dom"
 
 const Header = () => {
   return (
@@ -7,7 +8,7 @@ const Header = () => {
       <header className="menu">
         <h1 className="logo"> Vte </h1>
         <h1 className="welcome">Welcome to Vte!!</h1>
-        <a href="#">Login</a>      
+        <NavLink to="/Login">Login</NavLink>      
       </header>
     </div>
   );

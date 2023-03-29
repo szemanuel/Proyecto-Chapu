6907db8aa16a00350cfcb836e36dbe790fec38f1
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-03-29 14:27:54 -0300

    1 file modified, 4 added

--- src/App.js
+++ src/App.js
@@ -7,27 +7,14 @@ import { Footer } from './components/Footer/index';
 import { Login } from './pages/Login/index';
 import { Signup } from './pages/Signup/index';
 import { Plan_pricing } from './pages/Planpricing/index';
+import { Welcome } from './pages/Welcome/index';
+
 
 export default function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Body />
-        <Footer />
-        <Routes>
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/Plan_pricing" element={<Plan_pricing />} />
-        </Routes>
-      </BrowserRouter>
-
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-      {/* <Route path="/welcome"> <Welcome /></Route> */}
-    </div>
+      <Welcome />
+      
+     </div>
   );
 }
---
+++ src/pages/BodyChapu/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './BodyChapu.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Signup } from '../Signup/index';
+
+const BodyChapu = () => {
+  return (
+    <div className="loginPage">
+      <Route path="/welcome">
+        <Welcome />
+        </Route>
+      <Route path="/contact">
+        <Contact />
+        </Route>
+        <Route path="/me">
+        <Me />
+        </Route>
+        <Route path="/home">
+        <Home />
+        </Route>
+        <Route >
+          //agregar el resto de las páginas acá
+        <NoMatch />
+        </Route>
+        </div>
+
+  );
+};
+
+export { Login };
